5838a5babcdf7c18b6275ff6bd2963689e54cd3c
MarshallRdgz <marshalltankian@gmail.com>
2021-12-08 15:04:07 -0600

    feat: all files

---
+++ src/root.component.tsx
@@ -0,0 +1,10 @@
+/* export default function Root(props) {
+  return <section>{props.name} soy marshall haciendo cosas raras</section>;
+}
+*/
+
+const Root = () => {
+  return <div>Hola soy marshall</div>;
+};
+
+export default Root;
